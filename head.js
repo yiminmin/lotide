5aeaf54898af26ce9c072d6bf6c89eb629ccf958
yiminmin <emily141@163.com>
2023-04-14 19:27:56 +0000

    add head funtion and tail function to lOTIDE PROJECT

---
+++ head.js
@@ -0,0 +1,25 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+//Function implementation
+const head = function(array) {
+  return array[0];
+}
+
+//TEST CODE
+assertEqual(head([5,6,7],5));
+assertEqual(head(["Hello", "Lighthouse","Labs"], "Hello"));
+
+
+
+
---
+++ tail.js
@@ -0,0 +1,23 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+// function tail
+const tail = function(array) {
+  return array.slice(1);
+};
+
+ 
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!

1f6dd5e608613f058c719984828c1abc8aec5e37
yiminmin <emily141@163.com>
2023-04-28 14:11:25 +0000

    add findKeyByValue.js and letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,20 @@
+const letterPosition = function(sentence) {
+   const positions = {};
+
+   for (let i = 0; i < sentence.length; i++) {
+    const letter = sentence[i];
+    if (letter !== " ") {
+      if (positions[letter]) {
+        positions[letter].push(i);
+      } else {
+        positions[letter] = [i];
+      }
+    }
+   }
+
+   return positions;
+};
+
+const sentence = "lighthouse in the house";
+const result = letterPosition(sentence);
+console.log(result);

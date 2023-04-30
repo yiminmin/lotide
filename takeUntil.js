b9d67fefcea03927d24e732cb892bd56255a6988
yiminmin <emily141@163.com>
2023-04-30 01:23:32 +0000

    add takeUntil.js

---
+++ takeUntil.js
@@ -0,0 +1,34 @@
+// Define the takeUntil function, which takes an array and a callback function as parameters
+//这个函数是返回 callback  true 之前的所有false的值。
+const takeUntil = function(array, callback) {
+  // Create an empty results array to hold the collected elements
+  const results = [];
+  // Iterate over each element in the input array
+  for (let item of array) {
+    // If the callback function returns true for the current item,
+    // stop the loop and return the results array
+    /**
+     *  如果回调函数对当前元素返回 true，停止循环并返回结果数
+     */
+    if (callback (item)) {
+      return results;
+    }
+    // If the callback function returns a falsey value, add the current item to the results array
+    results.push(item);
+  }
+  // If the loop finishes without the callback function returning true,
+  // return the results array with all the items from the input array
+  return results;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);                
